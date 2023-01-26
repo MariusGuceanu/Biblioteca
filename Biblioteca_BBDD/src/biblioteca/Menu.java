8aece1dfa3c22c384e6b2f24681c8ec9bd842c0e
Marius Guceanu <Marius Guceanu@HZ114828.H012108.NET>
2023-01-26 12:23:21 +0100

    Creado el menu vacio

--- Biblioteca_BBDD/src/biblioteca/Menu.java
+++ Biblioteca_BBDD/src/biblioteca/Menu.java
@@ -1,5 +1,50 @@
 package biblioteca;
 
+import java.util.Scanner;
+
 public class Menu {
 
+	public static void main (String [] args) {
+		final int OPCION_LIBROS = 1;
+		final int OPCION_SOCIOS = 2;
+		final int OPCION_PRESTAMOS = 3;
+		final int SALIR = 0;
+
+		Scanner scan = new Scanner(System.in);
+		int opcion_menu;
+
+		do {
+			System.out.println("------MENU-------");
+			System.out.println(OPCION_LIBROS + ". Gestionar libros");
+			System.out.println(OPCION_SOCIOS + ". Gestionar socios");
+			System.out.println(OPCION_PRESTAMOS + ". Gestionar prestamos");
+			System.out.println(SALIR + ". Salir");
+			System.out.println("Elije una de las opciones");
+			opcion_menu = Integer.parseInt(scan.nextLine());
+
+			switch (opcion_menu) {
+			case OPCION_LIBROS:
+				System.out.println("Gestion de libros\n");
+				System.out.println("¿Que desea hacer?");
+				break;
+			case OPCION_SOCIOS:
+				System.out.println("Gestion de socios\n");
+				System.out.println("¿Que desea hacer?");
+				break;
+			case OPCION_PRESTAMOS:
+				System.out.println("Gestion de prestamos\n");
+				System.out.println("¿Que desea hacer?");
+				break;
+			case SALIR:
+				System.out.println("ADIOS");
+				break;
+			default:
+				System.out.println("Opcion incorrecta!");
+			}
+
+		} while (opcion_menu != SALIR);
+		scan.close();
+
+	}
+	
 }
