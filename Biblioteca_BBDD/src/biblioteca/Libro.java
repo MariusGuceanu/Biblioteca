fedc20923ca27122e40b36dc4c28abe23042eb38
Marius Guceanu <Marius Guceanu@HZ114828.H012108.NET>
2023-01-30 14:04:21 +0100

    constructores y getters/setters de las clases libro, prestamo y socio

--- Biblioteca_BBDD/src/biblioteca/Libro.java
+++ Biblioteca_BBDD/src/biblioteca/Libro.java
@@ -2,4 +2,44 @@ package biblioteca;
 
 public class Libro {
 
+	public Libro() {
+		
+	}
+	
+	public Libro(int id, String titulo, String autor, int num_Pag) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.num_Pag = num_Pag;
+	}
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public int getNum_Pag() {
+		return num_Pag;
+	}
+	public void setNum_Pag(int num_Pag) {
+		this.num_Pag = num_Pag;
+	}
+	private String titulo;
+	private String autor;
+	private int num_Pag;
+	
 }
--- Biblioteca_BBDD/src/biblioteca/Prestamo.java
+++ Biblioteca_BBDD/src/biblioteca/Prestamo.java
@@ -1,5 +1,8 @@
 package biblioteca;
 
 public class Prestamo {
-
+	private int id_libro;
+	private int id_socio;
+	private boolean devuelto;
+	
 }
