a2d2da17dbdf0ce593219cb3a4aa6ac2eb27f3d7
Marius Guceanu <Marius Guceanu@HZ114828.H012108.NET>
2023-01-30 13:30:08 +0100

    Creadas e implementadas (por ahora vacias) las clases de GsetorLibros/Socios y Visor

---
+++ Biblioteca_BBDD/src/biblioteca/GestorSocios.java
@@ -0,0 +1,46 @@
+package biblioteca;
+
+import java.util.Scanner;
+
+public class GestorSocios {
+
+	public static void run () {
+		final int OPCION_UNO = 1;
+		final int OPCION_DOS = 2;
+		final int OPCION_TRES = 3;
+		final int SALIR = 0;
+
+		Scanner scan = new Scanner(System.in);
+		int opcion_menu;
+
+		do {
+			System.out.println("------MENU-------");
+			System.out.println(OPCION_UNO + ". primera opcion");
+			System.out.println(OPCION_DOS + ". segunda opcion");
+			System.out.println(OPCION_TRES + ". tercera opcion");
+			System.out.println(SALIR + ". Salir");
+			System.out.println("Elije una de las opciones");
+			opcion_menu = Integer.parseInt(scan.nextLine());
+
+			switch (opcion_menu) {
+			case OPCION_UNO:
+				System.out.println("primera opcion seleccionada\n");
+				break;
+			case OPCION_DOS:
+				System.out.println("segunda opcion seleccionada\n");
+				break;
+			case OPCION_TRES:
+				System.out.println("tercera opcion seleccionada\n");
+				break;
+			case SALIR:
+				System.out.println("ADIOS");
+				break;
+			default:
+				System.out.println("Opcion incorrecta!");
+			}
+
+		} while (opcion_menu != SALIR);
+		scan.close();
+
+	}
+}
--- Biblioteca_BBDD/src/biblioteca/Visor.java
+++ Biblioteca_BBDD/src/biblioteca/Visor.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 
 public class Visor {
 
-	static void mostrarLibros (ArrayList<Libro> libros) {
-		
+	public static void mostrarLibros(ArrayList<Libro> libros) {
+
 	}
-	
-	static void mostrarLibros (Libro libro) {
+
+	public static void mostrarLibros(Libro libro) {
 		System.out.println(libro);
 	}
-	
-	static void mostrarMensaje (String mensaje) {
+
+	public static void mostrarMensaje(String mensaje) {
 		System.out.println(mensaje);
 	}
 }
