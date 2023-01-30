0e8672e7ba061a93d6fb30669c53d7ad5187fc3f
Marius Guceanu <Marius Guceanu@HZ114828.H012108.NET>
2023-01-30 13:20:42 +0100

    clase visor vacia

---
+++ Biblioteca_BBDD/src/biblioteca/Visor.java
@@ -0,0 +1,18 @@
+package biblioteca;
+
+import java.util.ArrayList;
+
+public class Visor {
+
+	static void mostrarLibros (ArrayList<Libro> libros) {
+		
+	}
+	
+	static void mostrarLibros (Libro libro) {
+		System.out.println(libro);
+	}
+	
+	static void mostrarMensaje (String mensaje) {
+		System.out.println(mensaje);
+	}
+}
