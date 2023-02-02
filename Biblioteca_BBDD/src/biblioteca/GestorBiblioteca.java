4b7c400d4c250a3e73a4c31ec2dba925ad45faf0
Marius Guceanu <Marius Guceanu@HZ114828.H012108.NET>
2023-02-02 10:36:20 +0100

    Menu (gestion de libros) creado y metodos implementados, por hacer: el resto de metodos de gestor libros y gestor socios

--- Biblioteca_BBDD/src/biblioteca/GestorBiblioteca.java
+++ Biblioteca_BBDD/src/biblioteca/GestorBiblioteca.java
@@ -5,6 +5,6 @@ import java.sql.SQLException;
 public class GestorBiblioteca {
 
 	public static void main (String[]args) throws ClassNotFoundException, SQLException {
-		GestorLibros.run();
+		Menu.mostrarMenuPrincipal();
 	}
 }
--- Biblioteca_BBDD/src/biblioteca/Menu.java
+++ Biblioteca_BBDD/src/biblioteca/Menu.java
@@ -1,5 +1,6 @@
 package biblioteca;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Menu {
@@ -14,11 +15,31 @@ public class Menu {
 	public static final int ELIMINAR_LIBRO = 2;
 	public static final int MODIFICAR_LIBRO = 3;
 	
-	public static void mostrarMenuPrincipal() {
+	public static void mostrarMenuPrincipal() throws ClassNotFoundException, SQLException {
+		Menu menu = new Menu();
+		Scanner scan = new Scanner (System.in);
+		int opcion;
+		do {
 		System.out.println("---Menu Principal---");
 		System.out.println("Gestionar libros");
 		System.out.println("Gestionar socios");
 		System.out.println("Gestionar Prestamos");
+		opcion = Integer.parseInt(scan.nextLine());
+		switch (opcion) {
+		case GESTIONAR_LIBROS:
+			GestorLibros.run();
+			
+			break;
+		
+		case GESTIONAR_SOCIOS:
+			GestorSocios.run();
+			
+			break;
+			
+		case GESTIONAR_PRESTAMOS:
+			System.out.println("Confia");
+			}
+		}while (opcion != 0);
 	}
 	
 }
